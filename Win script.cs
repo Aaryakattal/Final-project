0f80e1214aede43456a8f87ca4aa56a24784c0e7
Aaryakattal <aaryakattal223@gmail.com>
2025-06-09 14:02:02 -0600

    Win script.cs
    For win script

---
+++ Win script.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class WinScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(1); 
+        }
+        if (collision.gameObject.name == "Cato")
+        {
+            SceneManager.LoadScene(2);
+        }
+//Make sure to build scene and make sure number values are matched up!//
+    }
+}
